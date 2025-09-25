9b02ce7f241dd0914bbf84578ee832a21e0224c7
Lucas Fabricio Monzon Languasco <monzonlucasfabricio@gmail.com>
2025-09-25 11:08:50 -0300

    - Add firebase-config file. - Add hidden word again to index.html

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_FkE4Dn7BhdWF0R2LL36WVM2xZb5KZV8",
+  authDomain: "bd-inarq.firebaseapp.com",
+  projectId: "bd-inarq",
+  storageBucket: "bd-inarq.firebasestorage.app",
+  messagingSenderId: "387150282750",
+  appId: "1:387150282750:web:9a95c9498a574deab38aaf",
+  measurementId: "G-SNPL64L2VJ"
+};
